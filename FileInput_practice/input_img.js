332456fb7306988ccb6a8d9fdf56b3586fd4f343
Bamjong <qjawhd100@naver.com>
2022-10-11 21:53:27 +0900

    파일 읽고 출력하기

---
+++ FileInput_practice/input_img.js
@@ -0,0 +1,17 @@
+const fileInput = document.getElementById("fileUpload");
+
+const handleFiles = (e) => {
+  const selectedFile = [...fileInput.files];
+  const fileReader = new FileReader();
+
+  fileReader.readAsDataURL(selectedFile[0]);
+
+  fileReader.onload = function () {
+    document.getElementById("previewImg").src = fileReader.result;
+  };
+};
+
+fileInput.addEventListener("change", handleFiles);
+
+
+// https://developer-talk.tistory.com/330
